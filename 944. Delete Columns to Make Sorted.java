8e7a15295599fd9a6d8b792cc811469a6ec7f99f
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-01-03 12:28:50 +0530

    POTD

---
+++ 944. Delete Columns to Make Sorted.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    public int minDeletionSize(String[] strs) 
+    {
+        int count=0;
+        for(int i=0;i<strs[0].length();i++)
+        {
+            for(int j=0;j<strs.length-1;j++)
+            {
+                if(strs[j].charAt(i)<=strs[j+1].charAt(i))
+                    continue;
+                else
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;   
+    }
+}
+
+//Time Complexity : O(m*n) where is array size and n is length of string
+//Space Complexity : O(1) 

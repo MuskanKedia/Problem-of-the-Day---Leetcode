92a6ec17f4175f9d3cb8aa1eb1558b6a769b269a
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-02-18 22:54:10 +0530

    POTD

---
+++ 226. Invert Binary Tree.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        final Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            final TreeNode node = queue.poll();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
+
+//Time Complexity : O(N)
+//Space Complexity : O(N)

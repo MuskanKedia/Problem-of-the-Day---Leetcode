2f308e1581c61e6458a46670e651a8d235a9f7b2
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-02-23 21:33:40 +0530

    POTD

---
+++ 502. IPO.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        PriorityQueue<Point> q1 = new PriorityQueue<>((a, b) -> (a.cap - b.cap));
+        PriorityQueue<Point> q2 = new PriorityQueue<>((a, b) -> (b.pro - a.pro));
+        for(int i = 0; i<Profits.length; i++){
+            q1.add(new Point(Profits[i], Capital[i]));
+        }
+        for(int i = 0; i<k; i++){
+            while(!q1.isEmpty() && q1.peek().cap<=W) q2.add(q1.poll());
+            if(q2.size()==0) break;
+            W += q2.poll().pro;
+        }
+        return W;
+    }
+    class Point{
+        int pro; 
+        int cap;
+        public Point(int p, int c){
+            this.pro = p;
+            this.cap = c;
+        }
+    }
+}
+
+//Time Complexity : O(NlogN)
+//Space Complexity : O(1)

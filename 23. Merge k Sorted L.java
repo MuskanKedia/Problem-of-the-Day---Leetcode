d69edcda6031fea45f1693df919899edb9b301c6
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-03-12 20:16:21 +0530

    POTD

---
+++ 23. Merge k Sorted L.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+        public ListNode mergeKLists(ListNode[] lists) 
+        {
+          if (lists==null||lists.length==0) 
+            return null;
+          PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length ,(a,b) -> a.val-b.val);
+          ListNode dummy = new ListNode(0);
+          ListNode tail=dummy;
+        
+          for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+            
+          while (!queue.isEmpty())
+          {
+            tail.next=queue.poll();
+            tail=tail.next;
+            
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
+
+//Time Complexity : O(NlogK) where N is the total number of nodes in all the linked lists and K is the number of linked lists.
+//Space Complexity : O(K)

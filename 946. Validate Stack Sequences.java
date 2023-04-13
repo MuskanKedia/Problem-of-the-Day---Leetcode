9f99e10448a5939f8388ad3d49552dfff2472d60
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-04-13 10:10:01 +0530

    POTD

---
+++ 946. Validate Stack Sequences.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public boolean validateStackSequences(int[] pushed, int[] popped) 
+    {
+        int i=0,j=0;
+        for(int val:pushed)
+        {
+            pushed[i++]=val;
+            while(i>0 && pushed[i-1]==popped[j])
+            {
+                i--;
+                j++;
+            }
+        }
+        return i==0;
+    }
+}
+
+//Time Complexity : O(N)
+//Space Complexity : O(1)

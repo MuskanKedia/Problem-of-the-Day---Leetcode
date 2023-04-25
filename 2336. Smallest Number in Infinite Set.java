e4ad3b86995d0b2899467a4a8f2a42bb8de613b2
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-04-25 21:19:53 +0530

    POTD

---
+++ 2336. Smallest Number in Infinite Set.java
@@ -0,0 +1,30 @@
+class SmallestInfiniteSet {
+    PriorityQueue<Integer> queue;
+    int current;
+
+    public SmallestInfiniteSet() {
+        queue = new PriorityQueue<>();
+        current = 1;
+    }
+
+    public int popSmallest() {
+        int result = current;
+
+        if (!queue.isEmpty() && queue.peek() < current)
+            result = queue.poll();
+        else
+            current++;
+
+        while (!queue.isEmpty() && queue.peek() == result)
+            queue.poll();
+
+        return result;
+    }
+
+    public void addBack(int num) {
+        queue.add(num);
+    }
+}
+
+//Time Complexity : O(logN)
+//Space Complexity : O(N)

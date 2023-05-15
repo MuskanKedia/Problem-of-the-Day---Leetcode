3f94375163ef3204b20ebbb42eeee6a454bdd9b7
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-05-15 19:34:34 +0530

    POTD

---
+++ 1721. Swapping Nodes in a Linked List.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {		
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode first = head, second = head;
+        
+		// Put fast (k-1) nodes after slow
+        for(int i = 0; i < k - 1; ++i)
+            fast = fast.next;
+            
+		// Save the node for swapping
+        first = fast;
+
+		// Move until the end of the list
+        while(fast.next != null) {
+			slow = slow.next;
+            fast = fast.next;
+        }
+        second = slow;
+		
+		// Swap values
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        
+        return head;
+    }
+}
+
+//Time Complexity : O(N)
+//Space Complexity : O(1)

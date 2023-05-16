bd4b6d4bb865d0ff7e2ee27012f52ec995bae6e1
Muskan Kedia <81368928+MuskanKedia@users.noreply.github.com>
2023-05-16 19:16:29 +0530

    POTD

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
+
+//Time Complexity : O(N)
+//Space Complexity : O(N)
